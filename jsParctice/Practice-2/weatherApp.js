b5f58f8b26ad5ef6c9e307e2f5b32ef1807f57d5
SohailDAD <dsohail866@gmail.com>
2025-06-20 23:54:34 +0500

    Completed the weather application with fetch

--- jsParctice/Practice-2/weatherApp.js
+++ jsParctice/Practice-2/weatherApp.js
@@ -17,5 +17,11 @@ async function  weatherData(){
 }
 
 function displayData(data){
-    document.getElementById("")
+    let weatherData = document.getElementById("displayData")
+    weatherData.innerHTML = `
+        <h2>City: ${data.name}</h2>
+        <h3>Temperature: ${data.main.temp}</h3>
+        <h3>Weather: ${data.weather[0].main}</h3>
+        <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"  width="100px" height="100px"/>
+    `
 }
