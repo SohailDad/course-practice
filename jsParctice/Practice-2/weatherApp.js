d53c35f4e699ffa7651ffdcc9a1e58795b671f61
SohailDAD <dsohail866@gmail.com>
2025-06-20 23:35:48 +0500

    weather app with fetch and weathermap

--- jsParctice/Practice-2/weatherApp.js
+++ jsParctice/Practice-2/weatherApp.js
@@ -1,8 +1,21 @@
 let apiKey = ""
 
 
-function weatherData(){
+async function  weatherData(){
     let textInput = document.getElementById("textInput").value
-    let url = ``
-console.log("City Name: ",textInput)
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${textInput}&appid=${apiKey}&units=metric`
+
+   await fetch(url)
+    .then((response)=>response.json())
+    .then((data)=>{
+        displayData(data)
+    })
+    .catch((error)=>console.error("Error : ",error))
+
+
+    console.log("City Name: ",textInput)
+}
+
+function displayData(data){
+    document.getElementById("")
 }
