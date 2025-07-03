f21cf35309b9a09291727688b45311c44cbbf2d9
SohailDAD <dsohail866@gmail.com>
2025-07-03 19:44:46 +0500

    chnage code with the help of Chatgpt

--- reactJsPractice/nursery-shopping/src/components/ShoppingCards.jsx
+++ reactJsPractice/nursery-shopping/src/components/ShoppingCards.jsx
@@ -1,89 +1,165 @@
-import React from 'react'
-import Navbar from './Navbar'
-import { useDispatch, useSelector } from 'react-redux'
-import plants from "../jsonData/airPlants.json"
-import { cardDelete, cardTotal, cardTotalDecrement, decrementTotalCardAmount, totalCardAmount } from '../counterSlice'
+// import React from 'react'
+// import Navbar from './Navbar'
+// import { useDispatch, useSelector } from 'react-redux'
+// import plants from "../jsonData/airPlants.json"
+// import { cardDelete, cardTotal, cardTotalDecrement, decrementTotalCardAmount, totalCardAmount } from '../counterSlice'
+
+// function ShoppingCards() {
+//     const total = useSelector((state) => state.counter.total)
+//     const indexCard = useSelector((state) => state.counter.index)
+//     const totalCard = useSelector((state)=>state.counter.cardT)
+
+
+//     const dispatch = useDispatch()
+//     const deleteCard = (index,price)=>{
+//         dispatch(cardDelete(index))
+//         dispatch(decrementTotalCardAmount(price))
+//     }
+
+//     const incrementBtn = (price)=>{
+//         dispatch(totalCardAmount(price))
+//         dispatch(cardTotal(price))
+//     }
+
+//     const decrementBtn = (price)=>{
+//         dispatch(decrementTotalCardAmount(price))
+//         dispatch(cardTotalDecrement(price))
+//     }
+
+//     return (
+//         <>
+//             <Navbar />
+//             <div className='container mt-3 d-flex align-items-center flex-column '>
+//                 <h1 className='mb-4 '>Total Cards Amount : ${total}</h1>
+//                 {
+//                     indexCard.map((i) => {
+//                         const plant = plants[i-1];
+//                         return (
+//                             <div className="card  mb-1" style={{ maxWidth: "540px", }} key={plant.id}>
+//                                 <div className="row g-0">
+//                                     <div className="col-5">
+//                                         <img src={plant.image} className="img-fluid h-100 rounded-start" alt="..." />
+//                                     </div>
+//                                     <div className="col-7 px-5">
+//                                         <div className="card-body">
+//                                             <h5 className="card-title">{plant.name}</h5>
+//                                             <p className='fs-6'>Price : ${plant.price}</p>
+//                                             <div className='mb-2'>
+//                                                 <button className='px-2 border' 
+//                                                     onClick={()=>decrementBtn(plant.price)}
+//                                                 >
+//                                                     -
+
+//                                                 </button>
+//                                                 <span className='px-3' >1</span>
+//                                                 <button className=' border'
+//                                                     onClick={()=>incrementBtn(plant.price)}
+//                                                 >
+//                                                     +
+
+//                                                 </button>
+//                                             </div>
+//                                             <p className='fs-6'><b>Total : ${totalCard === 0 ? plant.price : totalCard}</b></p>
+//                                             <button className='btn btn-sm btn-danger'
+//                                                 onClick={()=>deleteCard(plant.id,plant.price) }
+//                                             >
+//                                                 Delete
+//                                             </button>
+//                                         </div>
+//                                     </div>
+
+//                                 </div>
+//                             </div>
+
+//                         )
+//                     })
+//                 }
+
+
+//                 <div className="d-grid">
+//                     <button className='btn btn-success mb-2 mt-4'>Continue Shopping</button>
+//                     <button className='btn btn-success mb-2 '>CheckOut</button>
+//                 </div>
+//             </div>
+//         </>
+
+//     )
+// }
+
+// export default ShoppingCards
+
+
+import React from 'react';
+import Navbar from './Navbar';
+import { useDispatch, useSelector } from 'react-redux';
+import plants from "../jsonData/airPlants.json";
+import {
+    cardDelete,
+    decrementQuantity,
+    incrementQuantity
+} from '../counterSlice';
+
+
 
 function ShoppingCards() {
-    const total = useSelector((state) => state.counter.total)
-    const indexCard = useSelector((state) => state.counter.index)
-    const totalCard = useSelector((state)=>state.counter.cardT)
+    const total = useSelector(state => state.counter.total);
+    const indexCard = useSelector(state => state.counter.index);
+    const quantities = useSelector(state => state.counter.quantities);
 
+    const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
-    const deleteCard = (index,price)=>{
-        dispatch(cardDelete(index))
-        dispatch(decrementTotalCardAmount(price))
-    }
+    const handleDelete = (id, price) => {
+        dispatch(cardDelete({id, price}));
+    };
 
-    const incrementBtn = (price)=>{
-        dispatch(totalCardAmount(price))
-        dispatch(cardTotal(price))
-    }
+    const handleIncrement = (id, price) => {
+        dispatch(incrementQuantity({ id, price }));
+    };
 
-    const decrementBtn = (price)=>{
-        dispatch(decrementTotalCardAmount(price))
-        dispatch(cardTotalDecrement(price))
-    }
+    const handleDecrement = (id, price) => {
+        dispatch(decrementQuantity({ id, price }));
+    };
 
     return (
         <>
             <Navbar />
-            <div className='container mt-3 d-flex align-items-center flex-column '>
-                <h1 className='mb-4 '>Total Cards Amount : ${total}</h1>
-                {
-                    indexCard.map((i) => {
-                        const plant = plants[i-1];
-                        return (
-                            <div className="card  mb-1" style={{ maxWidth: "540px", }} key={plant.id}>
-                                <div className="row g-0">
-                                    <div className="col-5">
-                                        <img src={plant.image} className="img-fluid h-100 rounded-start" alt="..." />
-                                    </div>
-                                    <div className="col-7 px-5">
-                                        <div className="card-body">
-                                            <h5 className="card-title">{plant.name}</h5>
-                                            <p className='fs-6'>Price : ${plant.price}</p>
-                                            <div className='mb-2'>
-                                                <button className='px-2 border' 
-                                                    onClick={()=>decrementBtn(plant.price)}
-                                                >
-                                                    -
-                                                    
-                                                </button>
-                                                <span className='px-3' >1</span>
-                                                <button className=' border'
-                                                    onClick={()=>incrementBtn(plant.price)}
-                                                >
-                                                    +
-                                                    
-                                                </button>
-                                            </div>
-                                            <p className='fs-6'><b>Total : ${totalCard === 0 ? plant.price : totalCard}</b></p>
-                                            <button className='btn btn-sm btn-danger'
-                                                onClick={()=>deleteCard(plant.id,plant.price) }
-                                            >
-                                                Delete
-                                            </button>
-                                        </div>
-                                    </div>
+            <div className='container mt-3 d-flex align-items-center flex-column'>
+                <h1 className='mb-4'>Total Cards Amount: ${total}</h1>
+                {indexCard.map((i) => {
+                    const plant = plants[i - 1];
+                    const quantity = quantities[i] || 1;
+                    const itemTotal = plant.price * quantity;
 
+                    return (
+                        <div className="card mb-1" style={{ maxWidth: "540px" }} key={plant.id}>
+                            <div className="row g-0">
+                                <div className="col-5">
+                                    <img src={plant.image} className="img-fluid h-100 rounded-start" alt="..." />
+                                </div>
+                                <div className="col-7 px-5">
+                                    <div className="card-body">
+                                        <h5 className="card-title">{plant.name}</h5>
+                                        <p className='fs-6'>Price: ${plant.price}</p>
+                                        <div className='mb-2'>
+                                            <button className='px-2 border' onClick={() => handleDecrement(plant.id, plant.price)}>-</button>
+                                            <span className='px-3'>{quantity}</span>
+                                            <button className='border' onClick={() => handleIncrement(plant.id, plant.price)}>+</button>
+                                        </div>
+                                        <p className='fs-6'><b>Total: ${itemTotal}</b></p>
+                                        <button className='btn btn-sm btn-danger' onClick={() => handleDelete(plant.id, plant.price)}>Delete</button>
+                                    </div>
                                 </div>
                             </div>
-
-                        )
-                    })
-                }
-
-
+                        </div>
+                    );
+                })}
                 <div className="d-grid">
                     <button className='btn btn-success mb-2 mt-4'>Continue Shopping</button>
-                    <button className='btn btn-success mb-2 '>CheckOut</button>
+                    <button className='btn btn-success mb-2'>CheckOut</button>
                 </div>
             </div>
         </>
-
-    )
+    );
 }
 
-export default ShoppingCards
+export default ShoppingCards;
--- reactJsPractice/nursery-shopping/src/counterSlice.js
+++ reactJsPractice/nursery-shopping/src/counterSlice.js
@@ -1,48 +1,116 @@
-import { createSlice } from '@reduxjs/toolkit'
+// import { createSlice } from '@reduxjs/toolkit'
 
+
+// export const counterSlice = createSlice({
+//     name: 'counter',
+//     initialState: {
+//         value: 0,
+//         total: 0,
+//         cardT: 0,
+//         index: []
+//     },
+//     reducers: {
+//         increment: state => {
+//             state.value += 1
+//         },
+//         totalCardAmount: (state, action) => {
+//             state.total += action.payload
+//         },
+
+//         cardIndex: (state, action) => {
+//             state.index.push(action.payload)
+//         },
+
+//         cardDelete: (state, action) => {
+//             state.index = state.index.filter(i => i !== action.payload)
+//             state.value -= 1
+//         },
+
+//         decrementTotalCardAmount: (state, action) => {
+//             if (state.total !== 0) {
+//                 state.total -= action.payload
+//             }
+//         },
+//         cardTotal: (state, action) => {
+//             state.cardT += action.payload
+//         },
+//         cardTotalDecrement: (state, action) => {
+//             if (state.cardT !== 0) {
+//                 state.cardT -= action.payload
+//             }
+//         }
+
+//     }
+// })
+
+// export const { increment, totalCardAmount, cardIndex, cardDelete, decrementTotalCardAmount, cardTotal, cardTotalDecrement } = counterSlice.actions
+
+// export default counterSlice.reducer
+
+
+
+import { createSlice } from '@reduxjs/toolkit';
 
 export const counterSlice = createSlice({
-    name: 'counter',
-    initialState: {
-        value: 0,
-        total: 0,
-        cardT: 0,
-        index: []
+  name: 'counter',
+  initialState: {
+    value: 0,
+    total: 0,
+    index: [],
+    quantities: {},   // { [id]: quantity }
+  },
+  reducers: {
+    increment: state => {
+      state.value += 1;
     },
-    reducers: {
-        increment: state => {
-            state.value += 1
-        },
-        totalCardAmount: (state, action) => {
-            state.total += action.payload
-        },
+    totalCardAmount: (state, action) => {
+      state.total += action.payload;
+    },
+    cardIndex: (state, action) => {
+      const id = action.payload;
+      if (!state.index.includes(id)) {
+        state.index.push(id);
+        state.quantities[id] = 1;
+      }
+    },
+    cardDelete: (state, action) => {
+      const id = action.payload.id;
+      const qty = state.quantities[id] || 0;
+      const plantPrice = action.payload.price ;
 
-        cardIndex: (state, action) => {
-            state.index.push(action.payload)
-        },
+    //   console.log("plantPrice : ",plantPrice)
+    //     console.log("id: ",id)
+    //     console.log("Total: ",state.total)
 
-        cardDelete: (state, action) => {
-            state.index = state.index.filter(i => i !== action.payload)
-            state.value -= 1
-        },
-
-        decrementTotalCardAmount: (state, action) => {
-            if (state.total !== 0) {
-                state.total -= action.payload
-            }
-        },
-        cardTotal: (state, action) => {
-            state.cardT += action.payload
-        },
-        cardTotalDecrement: (state, action) => {
-            if (state.cardT !== 0) {
-                state.cardT -= action.payload
-            }
-        }
-
+      state.total -= plantPrice * qty;
+      state.index = state.index.filter(i => i !== id);
+      delete state.quantities[id];
+      state.value -= 1;
+    },
+    incrementQuantity: (state, action) => {
+      const id = action.payload.id;
+      const price = action.payload.price;
+      state.quantities[id] += 1;
+      state.total += price;
+    },
+    decrementQuantity: (state, action) => {
+      const id = action.payload.id;
+      const price = action.payload.price;
+      if (state.quantities[id] > 1) {
+        state.quantities[id] -= 1;
+        state.total -= price;
+      }
     }
-})
+  }
+});
 
-export const { increment, totalCardAmount, cardIndex, cardDelete, decrementTotalCardAmount, cardTotal, cardTotalDecrement } = counterSlice.actions
+export const {
+  increment,
+  totalCardAmount,
+  cardIndex,
+  cardDelete,
+  incrementQuantity,
+  decrementQuantity
+} = counterSlice.actions;
 
-export default counterSlice.reducer
+export default counterSlice.reducer;
