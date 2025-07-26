ef84efa247d044939d9ae82f2f4f469bf551e2c1
SohailDAD <dsohail866@gmail.com>
2025-07-26 21:20:57 +0500

    create vite react app and install boostrap package

---
+++ addMassCRUD/frontend/src/App.jsx
@@ -0,0 +1,17 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="container p-4 text-center text-white bg-primary ">
+        <h1>  Welcome to Add Mass CRUD Application </h1>
+      </div>
+      <div>
+        {/* Add Items button */}
+        {/* Table */}
+      </div>
+    </>
+  )
+}
+
+export default App
