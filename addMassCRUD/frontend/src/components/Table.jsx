93309f321f093725a7ddcdfa52d62ce5dd6e4b89
SohailDAD <dsohail866@gmail.com>
2025-07-27 16:37:11 +0500

    work on logic or dynamic the table data

--- addMassCRUD/frontend/src/components/Table.jsx
+++ addMassCRUD/frontend/src/components/Table.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import axios from "axios"
 
 function Table() {
+    const [items,setItems] = useState([])
+
+    useEffect(() => {
+        
+        const data = axios.get("http://localhost:3000/")
+        console.log("Data: ",data);
+    
+      
+    }, [])
+    
+
     return (
         <div className='d-flex justify-content-center items-center container'>
-            <table className="table align-middle">
+            <table className="table align-middle table-responsive">
                 <thead>
                     <tr>
                         <th scope="col">Title</th>
@@ -13,19 +25,20 @@ function Table() {
                     </tr>
                 </thead>
                 <tbody>
+
                     <tr >
-                        <td>Mark</td>
+                        <td>Mark kdh qwh o oqwhd sohail dldqsd </td>
                         <td>1</td>
                         <td>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae itaque dolorum quidem quo explicabo sapiente voluptas facilis inventore sint? Ab iste doloribus placeat facilis amet id, repellat enim dolorum nobis?</td>
                          <td>
-                            <div className='d-flex justify-content-center '>
-                                <button className='btn btn-danger mx-2'>Delete</button>
+                            <div className='d-md-flex justify-content-center '>
+                                <button className='btn btn-danger mx-md-2'>Delete</button>
                                 <button className='btn btn-primary'>Edit</button>
                             </div>
                         </td>
 
                     </tr>
-                    <tr>
+                    {/* <tr>
                         <td>Jacob</td>
                         <td>0</td>
                         <td>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis odit vero, suscipit excepturi sunt ipsa laudantium dolorem, debitis eius aliquam officia quibusdam iste dignissimos quasi nemo. Quibusdam odio sit dolor.</td>
@@ -46,7 +59,7 @@ function Table() {
                                 <button className='btn btn-primary'>Edit</button>
                             </div>
                         </td>
-                    </tr>
+                    </tr> */}
                 </tbody>
             </table>
         </div>
